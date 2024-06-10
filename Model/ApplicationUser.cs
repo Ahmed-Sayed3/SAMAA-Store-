7a3835748312976de7cd0f7105bf1975c1f5521a
Ahmed Sayed <153014273+Ahmed-Sayed3@users.noreply.github.com>
2024-06-10 14:28:43 +0300

    Delete Model directory

--- Model/ApplicationUser.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Supply_Chain_And_Transportation.Model
-{
-    public class ApplicationUser : IdentityUser
-    {
-        public string Name { get; set; }
-        public string Address { get; set; }
-    }
-}
