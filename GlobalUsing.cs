63dff20241d4042476745b14351379f70f53d869
Ahmed Sayed <153014273+Ahmed-Sayed3@users.noreply.github.com>
2024-06-10 14:29:34 +0300

    Delete GlobalUsing.cs

--- GlobalUsing.cs
+++
@@ -1,6 +0,0 @@
-﻿// This  Class Contain All Using In Project And Make It Global To Be Readable Global Project 
-global using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-global using Supply_Chain_And_Transportation.Context;
-global using Supply_Chain_And_Transportation.Model;
-global using Microsoft.AspNetCore.Identity;
-global using Microsoft.EntityFrameworkCore;
